9719f1ef1972b5fc3bc2f18a009141e7d0b60f60
Your Name <sinan.haciboncuk@hotmail.com>
2022-09-01 22:31:34 +0300

    sout eklendi1

--- src/test/java/sinan.java
+++ src/test/java/sinan.java
@@ -1,8 +1,6 @@
 public class sinan {
     public static void main(String[] args) {
 
-
-        System.out.println("İşte Geliyor Özel HArekat...!!");
-
+        System.out.println("işte geliyor özel harekat...!!");
     }
 }
