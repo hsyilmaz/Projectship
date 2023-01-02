63b790c26eb15a02380019900a81713fdf150673
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2023-01-02 16:44:17 +0300

    credentials changed

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -12,14 +12,14 @@ public class LoginSteps {
 
     @Given("Navigate to basqar")
     public void navigateToBasqar() {
-        GWD.getDriver().get("https://demo.mersys.io/");
+        GWD.getDriver().get("https://test.mersys.io/");
         GWD.getDriver().manage().window().maximize();
     }
 
     @When("Enter username and password and click login button")
     public void enterUsernameAndPasswordAndClickLoginButton() {
-        dc.findAndSend("username","richfield.edu");
-        dc.findAndSend("password","Richfield2020!");
+        dc.findAndSend("username","turkeyts");
+        dc.findAndSend("password","TechnoStudy123");
         dc.findAndClick("loginButton");
     }
 
